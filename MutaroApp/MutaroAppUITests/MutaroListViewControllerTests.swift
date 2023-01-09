56f125eea13244d3ecdb5741b174d098f856427a
rei315 <alsrnr315@gmail.com>
2023-01-09 21:28:40 +0900

    remove test code

--- MutaroApp/MutaroAppUITests/MutaroListViewControllerTests.swift
+++ MutaroApp/MutaroAppUITests/MutaroListViewControllerTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 final class MutaroListViewControllerTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -23,18 +22,10 @@ final class MutaroListViewControllerTests: XCTestCase {
     }
 
     func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
 
     }
 
     func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+
     }
 }
