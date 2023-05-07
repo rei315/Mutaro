d89522da127896c7d93c02844b78bf4f88cb214c
rei315 <alsrnr315@gmail.com>
2023-05-07 22:39:42 +0900

    fix keychian

--- MutaroModule/Sources/KeychainStore/KeychainStore.swift
+++ MutaroModule/Sources/KeychainStore/KeychainStore.swift
@@ -12,62 +12,77 @@ public final class KeychainStore {
     public static let shared = KeychainStore()
 
     private let service = Bundle.main.bundleIdentifier ?? "Mutaro.com"
-    private let teamID = Bundle.main.object(forInfoDictionaryKey: "AppIdentifierPrefix") ?? "key"
-    private lazy var accessGroup = "\(teamID)\(service)"
 
-    public func save(_ element: some Codable, forKey key: String) throws -> Bool {
-        let encoder = JSONEncoder()
-        let infoData = try encoder.encode(element)
-        let query: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword,
-            kSecAttrAccount as String: key,
-            kSecAttrService as String: service,
-            kSecValueData as String: infoData,
-            kSecAttrAccessGroup as String: accessGroup
-        ]
-        let status = SecItemAdd(query as CFDictionary, nil)
-        return status == errSecSuccess
-    }
+    public func saveValue(_ value: some Codable, forKey key: KeychainStoreKey) throws {
+        let data = try JSONEncoder().encode(value)
 
-    public func get<T: Codable>(_: T.Type) throws -> [T] {
         let query: [CFString: Any] = [
             kSecClass: kSecClassGenericPassword,
             kSecAttrService: service,
-            kSecMatchLimit: kSecMatchLimitAll,
-            kSecReturnAttributes: true,
-            kSecReturnData: true,
-            kSecAttrAccessGroup: accessGroup
+            kSecAttrAccount: key.rawValue
         ]
+
+        let updateQuery: [CFString: Any] = [
+            kSecValueData: data
+        ]
+
+        let status = SecItemUpdate(query as CFDictionary, updateQuery as CFDictionary)
+
+        if status == errSecItemNotFound {
+            var fullQuery = query
+            fullQuery[kSecValueData] = data
+
+            let status = SecItemAdd(fullQuery as CFDictionary, nil)
+
+            if status != errSecSuccess {
+                throw KeychainError.saveError(status)
+            }
+        } else if status != errSecSuccess {
+            throw KeychainError.saveError(status)
+        }
+    }
+
+    public func loadValue<T: Codable>(forKey key: KeychainStoreKey) throws -> T {
+        let query: [CFString: Any] = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: key.rawValue,
+            kSecMatchLimit: kSecMatchLimitOne,
+            kSecReturnData: kCFBooleanTrue as Any
+        ]
+
         var result: AnyObject?
         let status = SecItemCopyMatching(query as CFDictionary, &result)
-        guard status == errSecSuccess else {
-            throw KeychainError.unhandledError(status: status)
+
+        guard status != errSecItemNotFound else {
+            throw KeychainError.loadError(status)
         }
-        guard let items = result as? [[CFString: Any]] else {
-            throw KeychainError.unexpectedData
+
+        guard status == errSecSuccess, let data = result as? Data else {
+            throw KeychainError.loadError(status)
         }
-        let decoder = JSONDecoder()
-        return try items.compactMap { item in
-            guard let itemData = item[kSecValueData] as? Data else {
-                return nil
-            }
-            return try decoder.decode(T.self, from: itemData)
-        }
+
+        let value = try JSONDecoder().decode(T.self, from: data)
+        return value
     }
 
-    @discardableResult
-    public func delete(_: (some Codable).Type) -> Bool {
+    public func deleteValue(forKey key: KeychainStoreKey) throws {
         let query: [CFString: Any] = [
             kSecClass: kSecClassGenericPassword,
             kSecAttrService: service,
-            kSecAttrAccessGroup: accessGroup
+            kSecAttrAccount: key.rawValue
         ]
+
         let status = SecItemDelete(query as CFDictionary)
-        return status == errSecSuccess || status == errSecItemNotFound
+
+        if status != errSecSuccess, status != errSecItemNotFound {
+            throw KeychainError.deleteError(status)
+        }
     }
 }
 
 enum KeychainError: Error {
-    case unhandledError(status: OSStatus)
-    case unexpectedData
+    case saveError(OSStatus)
+    case loadError(OSStatus)
+    case deleteError(OSStatus)
 }
---
+++ MutaroModule/Sources/KeychainStore/KeychainStoreKey.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//
+//
+//  Created by minguk-kim on 2023/05/07.
+//
+
+import Foundation
+
+public enum KeychainStoreKey: String {
+    case jwt = "jwt.mutaro.mghouse.com"
+}
