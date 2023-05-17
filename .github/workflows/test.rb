a6428e08beab788b3b77d734fb2f80c4d44ab15f
rei315 <alsrnr315@gmail.com>
2023-05-17 12:19:15 +0900

    fix

--- .github/workflows/test.rb
+++ .github/workflows/test.rb
@@ -2,6 +2,28 @@ require 'json'
 require 'net/http'
 require 'uri'
 
+def get_latest_tag(owner, repository, token)
+    url = "https://api.github.com/repos/#{owner}/#{repository}/tags"
+    
+    uri = URI(url)
+    request = Net::HTTP::Get.new(uri)
+    request['Authorization'] = "Bearer #{token}"
+    
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        http.request(request)
+    end
+    
+    if response.code == '200'
+        tags = JSON.parse(response.body)
+        latest_tag = tags[0]['name'] unless tags.empty?
+        latest_tag ||= 'N/A'
+    else
+        latest_tag = 'N/A'
+    end
+    
+    latest_tag
+end
+
 def parse_package_swift(file_path, dependencies)
   file_content = File.read(file_path)
 
@@ -41,6 +63,43 @@ def parse_package_swift(file_path, dependencies)
   dependencies
 end
 
+def extract_version(version)
+    version.gsub!(/^v/, '') if version.start_with?('v')
+    version.gsub!(/-.*/, '') if version.include?('-')
+    version = version.match(/\d+\.\d+\.\d+/) ? version.match(/\d+\.\d+\.\d+/)[0] : ""
+    version
+end
+
+def compare_versions(packageVersion, gitVersion)
+    v1_parts = packageVersion.split('.').map(&:to_i)
+    v2_parts = gitVersion.split('.').map(&:to_i)
+    
+    comparison_result = v1_parts <=> v2_parts
+    
+    return false if comparison_result == 1 || comparison_result == 0
+    return true
+end
+
+def check_available_new_version(token, data)
+    available_version_info = []
+    
+    data.each do |package|
+        owner, repository, version, url = package['owner'], package['repository'], package['version'], package['full_url']
+        latest_tag = get_latest_tag(owner, repository, token)
+        latest_tag = extract_version(latest_tag)
+        
+        result = compare_versions(version, latest_tag)
+        next if result == false
+        package_info = {}
+        package_info['currentVersion'] = version
+        package_info['newVersion'] = latest_tag
+        package_info['repository'] = url
+        available_version_info << package_info
+    end
+    
+    available_version_info
+end
+
 gh_token = ARGV[0]
 package_files = ARGV[1..]
 all_dependencies = []
@@ -48,5 +107,6 @@ all_dependencies = []
 package_files.each do |package_swift_file_path|
   parse_package_swift(package_swift_file_path, all_dependencies)
 end
+results = check_available_new_version(token, all_dependencies)
 
-puts JSON.generate(all_dependencies)
+puts JSON.generate(results)
