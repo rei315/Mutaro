84c1f382d25680759b4c36c2c2c77e4e7fe9f753
rei315 <alsrnr315@gmail.com>
2023-06-08 19:13:07 +0900

    fix

--- scripts/update-google-service-plist.sh
+++ scripts/update-google-service-plist.sh
@@ -9,26 +9,25 @@ GCM_SENDER_ID=$7
 GOOGLE_APP_ID=$8
 
 PATH=/usr/libexec:$PATH
-#${CI_WORKSPACE}/App/${CI_XCODE_PROJECT}/${CI_XCODE_PROJECT}
 
-if "${IS_DEV}"; then
-    if "${IS_CI}"; then
-        plistPath="${CI_WORKSPACE}/App/MutaroDev/MutaroDev"
-    else
-        plistPath="MutaroDev"
-    fi
-	plistFullPath="${plistPath}/Resources/GoogleService-Info.plist"
-else
-    if "${IS_CI}"; then
-        plistPath="${CI_WORKSPACE}/App/Mutaro/Mutaro"
-    else
-        plistPath="Mutaro"
-    fi
-	plistFullPath="${plistPath}/Resources/GoogleService-Info.plist"
-fi
-
-PlistBuddy -c "Set :CLIENT_ID $CLIENT_ID" "${plistFullPath}"
-PlistBuddy -c "Set :API_KEY $API_KEY" "${plistFullPath}"
-PlistBuddy -c "Set :REVERSED_CLIENT_ID $REVERSED_CLIENT_ID" "${plistFullPath}"
-PlistBuddy -c "Set :GCM_SENDER_ID $GCM_SENDER_ID" "${plistFullPath}"
-PlistBuddy -c "Set :GOOGLE_APP_ID $GOOGLE_APP_ID" "${plistFullPath}"
+#if "${IS_DEV}"; then
+#    if "${IS_CI}"; then
+#        plistPath="${CI_WORKSPACE}/App/MutaroDev/MutaroDev"
+#    else
+#        plistPath="MutaroDev"
+#    fi
+#	plistFullPath="${plistPath}/Resources/GoogleService-Info.plist"
+#else
+#    if "${IS_CI}"; then
+#        plistPath="${CI_WORKSPACE}/App/Mutaro/Mutaro"
+#    else
+#        plistPath="Mutaro"
+#    fi
+#	plistFullPath="${plistPath}/Resources/GoogleService-Info.plist"
+#fi
+#
+#PlistBuddy -c "Set :CLIENT_ID $CLIENT_ID" "${plistFullPath}"
+#PlistBuddy -c "Set :API_KEY $API_KEY" "${plistFullPath}"
+#PlistBuddy -c "Set :REVERSED_CLIENT_ID $REVERSED_CLIENT_ID" "${plistFullPath}"
+#PlistBuddy -c "Set :GCM_SENDER_ID $GCM_SENDER_ID" "${plistFullPath}"
+#PlistBuddy -c "Set :GOOGLE_APP_ID $GOOGLE_APP_ID" "${plistFullPath}"
