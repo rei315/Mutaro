1d56100dabbbf8d261586e646c4019214cbddf2f
rei315 <alsrnr315@gmail.com>
2023-06-08 16:46:00 +0900

    fix

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -8,7 +8,7 @@
 
 if [[ $CI_WORKFLOW = "Archive-For-Testflight-Develop" ]]; then
     PATH=/usr/libexec:$PATH
-    mainInfoPlist="${CI_WORKSPACE}/App/${CI_XCODE_PROJECT}/${CI_XCODE_PROJECT}/Resources/Info.plist"
+    mainInfoPlist="${CI_WORKSPACE}/App/${CI_PRODUCT}/${CI_PRODUCT}/Resources/Info.plist"
     
     nextBuildNumber=$CI_BUILD_NUMBER
     originalVersion=$(PlistBuddy -c "print CFBundleShortVersionString" "${mainInfoPlist}")
@@ -22,7 +22,7 @@ if [[ $CI_WORKFLOW = "Archive-For-Testflight-Develop" ]]; then
 elif [[ $CI_WORKFLOW = "Archive-For-Release" ]]; then
     PATH=/usr/libexec:$PATH
     nextBuildNumber=$CI_BUILD_NUMBER
-    mainInfoPlist="${CI_WORKSPACE}/App/${CI_XCODE_PROJECT}/${CI_XCODE_PROJECT}/Resources/Info.plist"
+    mainInfoPlist="${CI_WORKSPACE}/App/${CI_PRODUCT}/${CI_PRODUCT}/Resources/Info.plist"
     
     echo "Mutaro: - versionNumber is ${nextBuildNumber}"
     
