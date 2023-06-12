0221eeecf28a489f0100c9410c915e15cdc5cb65
rei315 <alsrnr315@gmail.com>
2023-06-12 13:37:05 +0900

    add

---
+++ MutaroModule/Sources/Core/Extensions/Concurrency/Publisher+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Publisher+Extensions.swift
+//  
+//
+//  Created by minguk-kim on 2023/06/12.
+//
+
+import Combine
+
+public extension Publisher {
+    func asyncMap<T>(
+        _ transform: @escaping (Output) async -> T
+    ) -> Publishers.FlatMap<Future<T, Never>, Self> {
+        flatMap { value in
+            Future { promise in
+                Task {
+                    let output = await transform(value)
+                    promise(.success(output))
+                }
+            }
+        }
+    }
+}
