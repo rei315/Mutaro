72ab5d3a0f6c3da0ddb72ee9bf3e85572ef3ce4a
rei315 <alsrnr315@gmail.com>
2024-02-19 23:49:22 +0900

    fix setting

--- MutaroModule/Package.swift
+++ MutaroModule/Package.swift
@@ -95,8 +95,7 @@ if ProcessInfo.processInfo.environment["CI"] == "TRUE" {
 let package = Package(
     name: "MutaroModule",
     platforms: [
-        .iOS(.v15),
-        .macOS(.v13)
+        .iOS(.v15)
     ],
     products: [
         .library(name: "Development", targets: ["Development"]),
