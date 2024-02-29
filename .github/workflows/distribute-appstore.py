6da5b91dbf089bf21dac9a6e9c89b1f264091a3a
rei315 <alsrnr315@gmail.com>
2024-03-01 00:03:24 +0900

    fix ci

--- .github/workflows/distribute-appstore.py
+++ .github/workflows/distribute-appstore.py
@@ -1,22 +1,16 @@
 import requests
 import os
+from enum import Enum
 
-def get(url, token):
-    headers = {
-        'Authorization': 'Bearer {}'.format(token),
-        'content-type': 'application/json'
-    }
-    response = requests.get(url, headers=headers)
-    return response
-    
-def post(url, token, body):
-    headers = {
-        'Authorization': 'Bearer {}'.format(token),
-        'content-type': 'application/json'
-    }
-    response = requests.post(url, headers=headers, json=body)
-    return response
+class BuildType(Enum):
+    scmGitReferences = 1
+    scmPullRequests = 2
 
+build_type = BuildType.scmGitReferences
+repository_id = None
+product_id = None
+workflows_list = None
+APP_STORE_URL = "https://api.appstoreconnect.apple.com/v1/"
 REPOSITORY_NAME = "Mutaro"
 
 JWT_TOKEN = os.environ['JWT_TOKEN']
@@ -24,71 +18,177 @@ BRANCH_NAME = os.environ['BRANCH_NAME']
 WORKFLOW_NAME = os.environ['WORKFLOW_NAME']
 TARGET_PRODUCT_NAME = os.environ['TARGET_PRODUCT_NAME']
 
-productsResponse = get("https://api.appstoreconnect.apple.com/v1/apps", JWT_TOKEN)
-if productsResponse.status_code != 200:
-    print(productsResponse.status_code)
-    exit()
+def get(url, token):
+    headers = {
+        'Authorization': 'Bearer {}'.format(token),
+        'content-type': 'application/json'
+    }
+    response = requests.get(APP_STORE_URL+url, headers=headers)
+    return response
     
-productsResponseJson = productsResponse.json()
-productsTargetData = productsResponseJson["data"]
-productsTargetCIProduct = [x for x in productsTargetData if x["attributes"]["name"] == TARGET_PRODUCT_NAME]
-productsTargetCIProductLink = productsTargetCIProduct[0]["relationships"]["ciProduct"]["links"]["related"]
+def post(url, token, body):
+    headers = {
+        'Authorization': 'Bearer {}'.format(token),
+        'content-type': 'application/json'
+    }
+    response = requests.post(APP_STORE_URL+url, headers=headers, json=body)
+    return response
 
-targetCIProductDetailResponse = get(productsTargetCIProductLink, JWT_TOKEN)
-if targetCIProductDetailResponse.status_code != 200:
-    print(targetCIProductDetailResponse.status_code)
-    exit()
+def get_repositories():
+    response = get("scmRepositories", JWT_TOKEN).json()["data"]
+    repositories = []
+    for repo in response:
+        repository = {
+            'id': repo['id'],
+            'name': repo['attributes']['repositoryName'],
+            'url': repo['attributes']['httpCloneUrl']
+        }
+        repositories.append(repository)
+    return repositories
 
-targetCIProductDetailJson = targetCIProductDetailResponse.json()
-targetCIProductDetailData = targetCIProductDetailJson["data"]
-targetCIWorkflowLink = targetCIProductDetailData["relationships"]["workflows"]["links"]["related"]
+def get_mutaro_repository_id():
+    global repository_id
+    if repository_id is None:
+        repositories = get_repositories()
+        repository = next((repo for repo in repositories if repo["name"] == REPOSITORY_NAME), None)
+        if repository:
+            repository_id = repository["id"]
+    return repository_id
 
-workflowListResponse = get(targetCIWorkflowLink, JWT_TOKEN)
-if workflowListResponse.status_code != 200:
-    print(workflowListResponse.status_code)
-    exit()
+def get_pull_requests(branch):
+    repository_id = get_mutaro_repository_id()
+    return get(f"scmRepositories/{repository_id}/pullRequests?limit=200&fields[scmPullRequests]=sourceBranchName", JWT_TOKEN).json()
 
-targetWorkflowJson = workflowListResponse.json()
-targetWorkflowData = targetWorkflowJson["data"]
-targetWorkflowId = [x for x in targetWorkflowData if x["attributes"]["name"] == WORKFLOW_NAME][0]["id"]
+def find_pr_request_for_branch(branch):
+    next_page = ''
+    pull_requests = get_pull_requests(branch)
+    pull_request_id = next((pullRequest["id"] for pullRequest in pull_requests["data"] if pullRequest["attributes"]["sourceBranchName"] == branch), None)
+    while pull_request_id is not None or next_page is None:
+        next_page = pull_requests.get("links", {}).get("next", "")
+        next_page = next_page.replace(APP_STORE_URL, '')
+        if not next_page:
+            break
+        pullRequest = get(next_page, JWT_TOKEN).json()
+        pull_request_id = next((pullRequest["id"] for pullRequest in pull_requests["data"] if pullRequest["attributes"]["sourceBranchName"] == branch), None)
+    return pull_request_id
 
-repositoriesResponse = get("https://api.appstoreconnect.apple.com/v1/scmRepositories", JWT_TOKEN)
-if repositoriesResponse.status_code != 200:
-    print(repositoriesResponse.status_code)
-    exit()
-    
-repositoriesJson = repositoriesResponse.json()
-targetRepositoryData = repositoriesJson["data"]
-targetRepositoryLink = [x for x in targetRepositoryData if x["attributes"]["repositoryName"] == REPOSITORY_NAME][0]["relationships"]["pullRequests"]["links"]["related"]
+def get_git_references():
+    repository_id = get_mutaro_repository_id()
+    return get(f"scmRepositories/{repository_id}/gitReferences?limit=200&fields[scmGitReferences]=kind,name", JWT_TOKEN).json()
 
-targetPullRequestResponse = get(targetRepositoryLink, JWT_TOKEN)
-if targetPullRequestResponse.status_code != 200:
-    print(targetPullRequestResponse.status_code)
-    exit()
+def find_git_reference_for_branch(branch):
+    next_page = ''
+    references = get_git_references()
+    branch_reference = next((reference["id"] for reference in references["data"] if reference["attributes"]["kind"] == "BRANCH" and reference["attributes"]["name"] == branch), None)
+    while branch_reference is not None or next_page is None:
+        next_page = references.get("links", {}).get("next", "")
+        next_page = next_page.replace(APP_STORE_URL, '')
+        if not next_page:
+            break
+        reference = get(next_page, JWT_TOKEN).json()
+        branch_reference = next((reference["id"] for reference in references["data"] if reference["attributes"]["kind"] == "BRANCH" and reference["attributes"]["name"] == branch), None)
+    return branch_reference
 
-targetPullRequestJson = targetPullRequestResponse.json()
-targetPullRequestData = targetPullRequestJson["data"]
-targetPullRequestId = [x for x in targetPullRequestData if x["attributes"]["sourceBranchName"] == BRANCH_NAME][0]["id"]
+def get_products():
+    products_data = get("ciProducts", JWT_TOKEN).json()["data"]
+    products = []
+    for product in products_data:
+        product_info = {
+            'id': product['id'],
+            'product': product['attributes']['name'],
+            'type': product['attributes']['productType']
+        }
+        products.append(product_info)
+    return products
 
-requestBuildBody = {
-    "data": {
-        "type": "ciBuildRuns",
-        "attributes": {},
-        "relationships": {
-            "workflow": {
-                "data": {
-                    "type": "ciWorkflows",
-                    "id": targetWorkflowId
-                }
-            },
-            "pullRequest": {
-                "data": {
-                    "type": "scmPullRequests",
-                    "id": targetPullRequestId
+def get_target_product_id():
+    global product_id
+    if product_id is None:
+        products = get_products()
+        product = next((p for p in products if p["product"] == TARGET_PRODUCT_NAME), None)
+        if product:
+            product_id = product["id"]
+    return product_id
+
+def get_workflows():
+    product_id = get_target_product_id()
+    return get(f"ciProducts/{product_id}/workflows?limit=200", JWT_TOKEN).json()["data"]
+
+def get_workflow_id_for_name(name):
+    global workflows_list
+    if workflows_list is None:
+        workflows_list = get_workflows()
+    workflow = next((w for w in workflows_list if w["attributes"]["name"].split("_")[0] == name), None)
+    if workflow:
+        return workflow["id"]
+    return None
+
+# scmPullRequests経由でビルド
+def start_build_by_pr(workflow_id):
+    pr_id = find_pr_request_for_branch(BRANCH_NAME)
+    body = {
+        'data': {
+            'type': 'ciBuildRuns',
+            'attributes': {},
+            'relationships': {
+                'workflow': {
+                    'data': {
+                        'type': 'ciWorkflows',
+                        'id': workflow_id
+                    }
+                },
+                'pullRequest': {
+                    'data': {
+                        'type': 'scmPullRequests',
+                        'id': pr_id
+                    }
                 }
             }
         }
     }
-}
-buildResponse = post("https://api.appstoreconnect.apple.com/v1/ciBuildRuns", JWT_TOKEN, requestBuildBody)
-print(buildResponse.status_code)
+    response = post("ciBuildRuns", JWT_TOKEN, body)
+    return response
+
+# scmGitReferences経由でビルド
+def start_build_by_branch(workflow_id):
+    branch_id = find_git_reference_for_branch(BRANCH_NAME)
+    body = {
+        'data': {
+            'type': 'ciBuildRuns',
+            'attributes': {},
+            'relationships': {
+                'workflow': {
+                    'data': {
+                        'type': 'ciWorkflows',
+                        'id': workflow_id
+                    }
+                },
+                'sourceBranchOrTag': {
+                    'data': {
+                        'type': 'scmGitReferences',
+                        'id': branch_id
+                    }
+                }
+            }
+        }
+    }
+    response = post("ciBuildRuns", JWT_TOKEN, body)
+    return response
+
+def main():
+    workflow_id = get_workflow_id_for_name(WORKFLOW_NAME)
+    if build_type == BuildType.scmPullRequests:
+        build_response = start_build_by_pr(workflow_id)
+    elif build_type == BuildType.scmGitReferences:
+        build_response = start_build_by_branch(workflow_id)
+    else:
+        build_response = None
+
+    if build_response is not None:
+        build_response.raise_for_status()
+        print(build_response.status_code)
+    else:
+        print("Error: need to set build_type")
+
+if __name__ == "__main__" :
+    main()
--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -26,23 +26,52 @@ get_app_version() {
     PlistBuddy -c "print CFBundleShortVersionString" "${mainInfoPlist}"
 }
 
+get_pull_request_id() {
+    url="https://api.github.com/repos/rei315/Mutaro/pulls?state=open&sort=created&direction=asc&per_page=100"
+
+    response=$(curl -s -L \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        "$url")
+
+    if [ $? -eq 0 ]; then
+        echo "$response" | tr -d '[:cntrl:]' | sed -E 's/\\([][])/\\\\\1/g' | jq -r ".[] | select(.head.ref == \"$CI_BRANCH\") | .number"
+    else
+        echo ""
+    fi
+}
+
 version_pr_comment() {
-    PR_NUMBER=$CI_PULL_REQUEST_NUMBER
+    PR_NUMBER=$1
     if [ ! -n $PR_NUMBER ]; then
         return 0
     fi
         
     APP_VERSION=$(get_app_version)
     
-    curl -X POST \
+    response=$(curl -X POST \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: token ${GITHUB_TOKEN}" \
     "https://api.github.com/repos/rei315/Mutaro/issues/$PR_NUMBER/comments" \
-    -d '{"body":"ビルドが成功し、下記のバージョンで配信中です。\nApp Version: '${APP_VERSION}'\nBuild Number: '${CI_BUILD_NUMBER}'"}'
+    -d '{"body":"ビルドが成功し、下記のバージョンで配信中です。\nApp Version: '${APP_VERSION}'\nBuild Number: '${CI_BUILD_NUMBER}'"}')
+    
+    if [ $? -ne 0 ]; then
+        return 0
+    fi
 }
 
 if [[ $CI_WORKFLOW = "Archive-For-Testflight-Develop" ]]; then
-    version_pr_comment
+    if [ -n "$CI_PULL_REQUEST_NUMBER" ]; then
+        PR_NUMBER="$CI_PULL_REQUEST_NUMBER"
+    else
+        PR_NUMBER=$(get_pull_request_id)
+    fi
+    if [ -n "$PR_NUMBER" ]; then
+        version_pr_comment $PR_NUMBER
+    else
+        echo "Mutaro: failed to comment build info"
+    fi
 elif [[ $CI_WORKFLOW = "Archive-For-Release" ]]; then
     upload_dsym
 fi
